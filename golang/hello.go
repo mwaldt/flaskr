9e147d7f5847ef4294b57444060f4db3a70cb67f
Max Waldt <waldm63@gmail.com>
2025-10-21 16:47:50 -0400

    golang: hello language support

--- golang/hello.go
+++ golang/hello.go
@@ -2,15 +2,35 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+    spanish = "Spanish"
+    french = "French"
+ 
+    englishHelloPrefix = "Hello, "
+    spanishHelloPrefix = "Hola, "
+    frenchHelloPrefix = "Bonjour, "
+)
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
     if name == "" {
         name = "World"
     }
-	return englishHelloPrefix + name
+	
+    return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+    switch language {
+    case spanish:
+        return spanishHelloPrefix
+    case french:
+        return frenchHelloPrefix
+    default:
+        return englishHelloPrefix
+    }
+    return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
